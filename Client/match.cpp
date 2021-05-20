9b19494cee39f21fc33546514557828c3051d3a6
guilherme <guitavares31@usp.br>
2021-05-19 22:12:05 -0300

    Added basic operations for the client side, such as compute a single play, check whether the game has a winner and print the board in the terminal

--- Client/match.cpp
+++ Client/match.cpp
@@ -1,17 +1,105 @@
-#include <stdio.h>
+#include <iostream>
+#include <string.h>
 #include "match.hpp"
 
 using namespace std;
 
+Match::Match(int _player) {
+    
+    // Sets all values of the board with 0(empty)
+    memset(this->board, 0, sizeof(this->board));
+    this->remainingPositions = 9;
+    
+
+    // TO-DO - Trow exception if != 1 && != -1
+    this->player = _player;
+}
+
 void Match::printBoard() {
     
     for(int i = 0; i < 3; i++) {
+        printf("-------------\n|");
         for(int j = 0; j < 3; j++) {
             
-            printf("%d ", this->board[i][j]);
+            printf(" %d |", this->board[i][j]);
         }
         printf("\n");
     }
+    printf("-------------\n|");
 
     return;
 }
+
+void Match::defineWinner() {
+
+    // Auxiliary variables to check the values...
+    int column_sum      = 0;
+    int row_sum         = 0;
+    int diagonal_sum[2] = {0,0};
+
+    for(int i = 0; i < 3; i++) {
+        diagonal_sum[0] += this->board[i][i];
+        diagonal_sum[1] += this->board[2-i][2-i];
+        for(int j = 0; j < 3; j++) {
+            column_sum += this->board[j][i];
+            row_sum    += this->board[j][i];
+        }
+
+        // Check if Nought is the winner
+        if(column_sum == 3 || row_sum == 3){
+            this->winner = NOUGHT;
+            return;
+        }
+        // Check if Cross is the winner
+        if(column_sum == -3 || row_sum == -3){
+            this->winner = CROSS;
+            return;
+        }
+
+        if(remainingPositions == 0){
+            this->winner = DRAW;
+            return;
+        }
+
+        this->winner = IN_PROGRESS;
+        return;
+    }
+
+    // Checking for diagonals...
+    if(diagonal_sum[0] == 3 || diagonal_sum[1] == 3){
+        this->winner = NOUGHT;
+        return;
+    }
+    if(diagonal_sum[0] == -3 || diagonal_sum[1] == -3){
+        this->winner = CROSS;
+        return;
+    }
+
+}
+
+int Match::makePlay() {
+    
+    int x, y;
+    bool validPosition = false;
+
+    if(this->player != CROSS && this->player != NOUGHT) {
+        
+        return 0;
+    }
+
+    while(!validPosition){
+
+        cout << "Escolha uma posição em que deseja jogar (Ex: '1 2'): \n";
+        cin >> x >> y;
+
+        if( x>=1 && x<=3 && y>=1 && y<=3 && this->board[y][x] == 0)
+            validPosition = true;
+        else{
+            validPosition = false;
+            cout << "Posição inválida, tente novamente.\n";
+        }
+    }
+    return 1;
+}
+
+
--- Client/match.hpp
+++ Client/match.hpp
@@ -4,27 +4,42 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define CROSS      -1
+#define NOUGHT      1
+#define DRAW        3
+#define IN_PROGRESS 4
+
+
 class Match {
 
-    private:
-        /* Stores the Board
+    private:        
+        // Contains with the local player is playing with CROSS or NOUGHT
+        int player;
+
+        /* Winner of the Match 
+            -1 - Cross  player
+             2 - Nought player
+             3 - Draw
+             4 - Match in progress
+        */  
+        int  winner;
+        int remainingPositions;
+
+    public:
+         /* Stores the Board
         
             !! - It can be also stored as a simple array.
         */
-        int board[3][3]; 
-
-        /* Winner of the Match 
-            1 - Player 1
-            2 - Player 2
-            3 - Draw
-            4 - Match in progress
-        */  
-        int winner;
-
-    public:
+        int board[3][3];
         
+        Match(int _player);
+
+        void  defineWinner();
+
         int** getBoard();
         int   getWinner();
+
+        int   makePlay();
 
         /* Prints the bord in the terminal */
         void printBoard();
